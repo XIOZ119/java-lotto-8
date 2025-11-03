75c9af3991e4f5ff269a8f80a700e875aefbd766
홍시은 <tjwns53@naver.com>
2025-11-03 09:09:06 +0900

    feat: WinningLotto 클래스 구현
    - 당첨 번호와 보너스 번호를 관리하는 클래스 생성
    - 보너스 번호 유효성 검증 (1~45 범위, 당첨 번호와 중복 불가)
    - 구매한 로또와 당첨 번호를 비교하여 등수 판별

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import lotto.enums.Rank;
+
+public class WinningLotto {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+        validateBonusNumber();
+    }
+
+    private void validateBonusNumber() {
+        validateBonusRange();
+        validateBonusDuplicate();
+    }
+
+    private void validateBonusRange() {
+        if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateBonusDuplicate() {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Rank match(Lotto lotto) {
+        int matchCount = lotto.countMatchingNumbers(winningNumbers.getNumbers());
+        boolean bonusMatch = lotto.contains(bonusNumber);
+        return Rank.of(matchCount, bonusMatch);
+    }
+}
